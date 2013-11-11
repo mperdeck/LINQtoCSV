f1b94d1ca83cc223e7d2f1ea3780b2e3d56cbc00
Mark Cottman-Fields <qut.bioacoustics.research+mark@gmail.com>
2013-11-11 16:57:17 +1000

    Added tests for parallel linq and DataRow.

---
+++ LINQtoCSV.Tests/DataRowAssertable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LINQtoCSV.Tests
+{
+    public class DataRowAssertable : List<DataRowItem>, IDataRow, IAssertable<DataRowAssertable>
+    {
+        public void AssertEqual(DataRowAssertable other)
+        {
+            Assert.AreNotEqual(other, null);
+            Assert.AreEqual(other.Count, this.Count);
+
+            for (var index = 0; index < this.Count; index++)
+            {
+                Assert.AreEqual(other[index].Value, this[index].Value, index.ToString());
+                Assert.AreEqual(other[index].LineNbr, this[index].LineNbr, index.ToString());
+            }
+        }
+    }
+}
