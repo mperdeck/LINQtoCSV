62acd5d0479a6792e06ecfa167af62429f1734d5
Valeriu <lacatusu.valeriu@gmail.com>
2014-01-16 18:07:18 +0100

    Updated unit tests for the different features combinations :  - UseOutputFormatForParsingCsvValue  - NoSeparatorChar  - UseFieldIndexForReadingData

--- LINQtoCSV.Tests/ProductDataSpecificFieldIndex.cs
+++ LINQtoCSV.Tests/ProductDataSpecificFieldIndex.cs
@@ -14,15 +14,15 @@ namespace LINQtoCSV.Tests
 
     internal class ProductDataSpecificFieldIndex : IAssertable<ProductDataSpecificFieldIndex>
     {
-        [CsvColumn(FieldIndex = 1, CharLength = 8)]
+        [CsvColumn(FieldIndex = 1)]
         public string name;
 
         // OutputFormat uses the same codes as the standard ToString method (search MSDN).
-        [CsvColumn(FieldIndex = 3, OutputFormat = "dd/MM/yyyy", CharLength = 8)]
+        [CsvColumn(FieldIndex = 4, OutputFormat = "MM/dd/yy")]
         public DateTime startDate;
 
         // Can use both fields and properties
-        [CsvColumn(FieldIndex = 2, CanBeNull = false, CharLength = 6)]
+        [CsvColumn(FieldIndex = 3, CanBeNull = false)]
         public double weight { get; set; }
 
 #pragma warning restore 0169, 0414, 0649
@@ -34,6 +34,51 @@ namespace LINQtoCSV.Tests
             Assert.AreEqual(other.name, name, "name");
             Assert.AreEqual(other.startDate, startDate, "startDate");
             Assert.AreEqual(other.weight, weight, "weight");
+        }
+    }
+
+    internal class ProductDataCharLength : IAssertable<ProductDataCharLength>
+    {
+        [CsvColumn(FieldIndex = 1, CharLength = 8)]
+        public string name;
+
+        // OutputFormat uses the same codes as the standard ToString method (search MSDN).
+        [CsvColumn(FieldIndex = 3, OutputFormat = "MM/dd/yy", CharLength = 8)]
+        public DateTime startDate;
+
+        // Can use both fields and properties
+        [CsvColumn(FieldIndex = 2, CanBeNull = false, CharLength = 6)]
+        public double weight { get; set; }
+
+#pragma warning restore 0169, 0414, 0649
+
+        public void AssertEqual(ProductDataCharLength other)
+        {
+            Assert.AreNotEqual(other, null);
+
+            Assert.AreEqual(other.name, name, "name");
+            Assert.AreEqual(other.startDate, startDate, "startDate");
+            Assert.AreEqual(other.weight, weight, "weight");
+        }
+    }
+
+    internal class ProductDataParsingOutputFormat : IAssertable<ProductDataParsingOutputFormat>
+    {
+        [CsvColumn(FieldIndex = 1)]
+        public string name;
+
+        // OutputFormat uses the same codes as the standard ToString method (search MSDN).
+        [CsvColumn(FieldIndex = 2, OutputFormat = "MMddyy")]
+        public DateTime startDate;
+
+#pragma warning restore 0169, 0414, 0649
+
+        public void AssertEqual(ProductDataParsingOutputFormat other)
+        {
+            Assert.AreNotEqual(other, null);
+
+            Assert.AreEqual(other.name, name, "name");
+            Assert.AreEqual(other.startDate, startDate, "startDate");
         }
     }
 }
