674fc6a1045ffb1fa932f62a83cfb7cf64c4a34c
Eamon Hetherton <eamon@global-roam.com>
2020-07-24 10:49:36 +1000

    fix build

--- Build/Build.cs
+++ Build/Build.cs
@@ -1,11 +1,19 @@
 using GlobalRoam.Build.Nuke;
+using Nuke.Common;
 using Nuke.Common.Execution;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
 using System;
 using System.Linq;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [CheckBuildProjectConfigurations]
 [UnsetVisualStudioEnvironmentVariables]
 class Build : NugetPackageBuild
 {
+	protected override bool IncludeSourceInPack => false;
+	protected override bool IncludeSymbolsInPack => false;
+
 	public static int Main() => Execute<Build>(x => x.Test);
+
 }
